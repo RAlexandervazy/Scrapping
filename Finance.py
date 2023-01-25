da1fad7a90e55da1d1d67db1e43b8ff24cde62b0
Rodrigo Alexander Vázquez Vázquez <bioalex1095@gmail.com>
2023-01-24 21:32:12 -0600

    Clean_code

--- Finance.py
+++ Finance.py
@@ -2,21 +2,16 @@ from bs4 import BeautifulSoup
 import cfscrape
 import time
 
-valor_final = []
-valorfinalUSD= []
-valorfinalEur= []
-valorfinaljpy=[]   #Yen Japo
-valorfinalcny=[]   #Yuan chino
-valorfinalOro=[]
-valorfinalPlata=[]
-valorfinalPetroleo=[]
 
 base_url = 'https://mx.investing.com'
 url_coins = 'currencies'
 url_commodities = 'commodities'
 
-coins_arr = ['usd-mxn', 'eur-mxn', 'jpy-mxn', 'cny-mxn']
 
+#usar funciones  - retorne arreglos para usar con cualquier moneda
+#Aplicar para comodities
+coins_arr = ['usd-mxn', 'eur-mxn', 'jpy-mxn', 'cny-mxn']
+#symbols_coins = {"usd-mxn":{"$US","1"},"eur-mxn":"€"} 
 scraper = cfscrape.create_scraper()
 
 for coin in coins_arr:
@@ -29,12 +24,6 @@ for coin in coins_arr:
     print(f'Valor de {coin}: {i.text}')
 
 
-#headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61'}
-#The url's are declared
-#urlusd = "https://mx.investing.com/currencies/usd-mxn"
-#pageusd = requests.get(urlusd, headers=headers)
-#soupusd = BeautifulSoup(pageusd.content,"html.parser")
-
 
 # scraperusd= scraper.get('https://mx.investing.com/currencies/usd-mxn')
 # scrapereur= scraper.get('https://mx.investing.com/currencies/eur-mxn')
@@ -43,42 +32,6 @@ for coin in coins_arr:
 # scraperOro= scraper.get('https://mx.investing.com/commodities/gold')
 # scraperPlata= scraper.get('https://mx.investing.com/commodities/silver')
 # scraperPetroleo= scraper.get('https://mx.investing.com/commodities/crude-oil')
-
-# soupusd = BeautifulSoup(scraperusd.content, "html.parser")
-# soupeur = BeautifulSoup(scrapereur.content, "html.parser")
-# soupjpy = BeautifulSoup(scraperjpy.content, 'html.parser')
-# soupcny = BeautifulSoup(scrapercny.content, 'html.parser')
-# soupOro = BeautifulSoup(scraperOro.content, 'html.parser')
-# soupPlata = BeautifulSoup(scraperPlata.content, 'html.parser')
-# soupPetroleo = BeautifulSoup(scraperPetroleo.content, 'html.parser')
-
-#     #Costo
-
-# Valorusd = soupusd.find_all("span", class_="text-2xl")
-# ValorEur = soupeur.find_all('span', class_="text-2xl")
-# Valorjpy = soupjpy.find_all('span', class_="text-2xl")
-# Valorcny = soupcny.find_all('span', class_="text-2xl")
-# ValorOro = soupOro.find_all('span', class_="text-2xl")
-# ValorPlata = soupPlata.find_all('span', class_="text-2xl")
-# ValorPetroleo = soupPetroleo.find_all('span', class_="text-2xl")
-
-# for i in Valorusd:
-#     valorfinalUSD.append(i.text)
-# for i in ValorEur:
-#     valorfinalEur.append(i.text)   
-# for i in Valorjpy:
-#     valorfinaljpy.append(i.text)
-# for i in Valorcny:
-#     valorfinalcny.append(i.text)
-# for i in ValorOro:
-#     valorfinalOro.append(i.text)
-# for i in ValorPlata:
-#     valorfinalPlata.append(i.text)
-# for i in ValorPetroleo:
-#     valorfinalPetroleo.append(i.text)
-
-
-# print(("Dolar {}, Euro {}, Yen japones {}, Yuan Chino {}, Oro {}, Plata {}, Petroleo {}").format(valorfinalUSD,valorfinalEur,valorfinaljpy,valorfinalcny, valorfinalOro, valorfinalPlata, valorfinalPetroleo))
 
 time.sleep(10)   
    
