f08ba47fd1a5e4e1ec35dcbf1d08fa2f9e0ee8e0
Rodrigo Alexander Vázquez Vázquez <bioalex1095@gmail.com>
2023-02-10 02:35:02 -0600

    creacion de documento

--- Finance.py
+++ Finance.py
@@ -8,36 +8,17 @@ commodities = Scrapping('commodities')
 
 resultado_coins = coins.makeScrapping()
 resultado_comoddities = commodities.makeScrapping()
+print(resultado_coins)
+print(resultado_comoddities)
 
-coins.changeStatic('ars-mxn')
-coins.makeScrapping()
-
-#Debuggear porque no devuelve valores
-bitcoin = Scrapping('crypto', ['bitcoin'])
-print(bitcoin.makeScrapping())
-
-#usar funciones  - retorne arreglos para usar con cualquier moneda
-#Aplicar para comodities
+cwd = coins.NewDoc()
+coins.InsertData(cwd,resultado_comoddities)
 
 
-#Version 1.2 se genera la funcion con el tipo de variable a consultar
-
-
-# def scrapValor(base_url,section_url,specific_item,label,class_):
-#   URL = f'{base_url}/{section_url}/{specific_item}'
-#   scraperURL = scraper.get(URL)
-#   soupURL = BeautifulSoup(scraperURL.content, "html.parser")
-#   valorURL = soupURL.find_all(label, class_)
-#   for i in valorURL:
-#     return f'Valor de {specific_item}: {i.text}'
-
-# print('Sepracion***********************')
-
-# for coin in coins_arr:
-#   print(scrapValor(base_url,url_coins,coin,"span","text-2xl"))
-
-# for commodi in commodities_arr:
-#   print(scrapValor(base_url,url_commodities,commodi,"span","text-2xl"))
+#coins.changeStatic('ars-mxn')
+#coins.makeScrapping()
+#bitcoin = Scrapping('indices', ['investing.com-btc-mxn'])
+#print(bitcoin.makeScrapping())
 
 time.sleep(5)   
    
--- clases.py
+++ clases.py
@@ -1,34 +1,60 @@
 from bs4 import BeautifulSoup
 import cfscrape
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import os
+from pathlib import Path
 
 
 class Scrapping:
-    coins_arr = ['usd-mxn', 'eur-mxn', 'jpy-mxn', 'cny-mxn']
+    coins_arr = ['usd-mxn','eur-mxn','jpy-mxn','cny-mxn']
     commodities_arr = ["gold","silver","crude-oil"]
     scraper = cfscrape.create_scraper()
 
     def __init__(self, endpoint, endpoint_arr = [], base_url= 'https://mx.investing.com', label="span", html_class="text-2xl"):
-        self.base_url = base_url
-        self.endpoint = endpoint
-        self.label = label
-        self.html_class = html_class
-        self.resultado = []
-        if endpoint == 'commodities': 
-          self.specific_item_arr = self.commodities_arr
-        elif endpoint == 'coins':
-          self.specific_item_arr = self.coins_arr
-        else:
-          self.specific_item_arr = endpoint_arr
+      self.base_url = base_url
+      self.endpoint = endpoint
+      self.label = label
+      self.html_class = html_class
+      self.resultado = []
+      if endpoint == 'commodities': 
+        self.specific_item_arr = self.commodities_arr
+      elif endpoint == 'currencies':
+        self.specific_item_arr = self.coins_arr
+      else:
+        self.specific_item_arr = endpoint_arr
           
     def makeScrapping(self):
-        for item in self.specific_item_arr:
-          URL=f'{self.base_url}/{self.endpoint}/{item}'
-          scraperURL = self.scraper.get(URL)
-          soupURL = BeautifulSoup(scraperURL.content, "html.parser")
-          valorURL = soupURL.find_all(self.label, self.html_class)
-          for i in valorURL:
-            self.resultado.append(i.text)
-        return self.resultado
+      for item in self.specific_item_arr:
+        URL=f'{self.base_url}/{self.endpoint}/{item}'
+        scraperURL = self.scraper.get(URL)
+        soupURL = BeautifulSoup(scraperURL.content, "html.parser")
+        valorURL = soupURL.find_all(self.label, self.html_class)
+        for i in valorURL:          
+          self.resultado.append(i.text)
+      return self.resultado
     
     def changeStatic(self, new_value):
-        self.coins_arr = self.coins_arr.append(new_value)
+      self.coins_arr = self.coins_arr.append(new_value)
+
+    def NewDoc(self):
+      if os.path.isfile('Valores.xlsx'):
+        cwd=Path.cwd()
+      else:
+        doc = Workbook()
+        Mainsheet = doc.active
+        Mainsheet.title="ValoresDB"
+        doc.save('Valores.xlsx')
+        cwd=Path.cwd()
+        return cwd
+    
+    def InsertData(self,cwd,data,colum='A',row=1):
+      doc = load_workbook(f'{cwd}/Valores.xlsx')
+      Mainsheet = doc.active
+      for i in data:
+        Mainsheet[f'{colum}{row}']=i
+        row+=1
+        doc.save('Valores.xlsx')
+
+       
+
